68d2b6fbf11edebcb891e4725f48b737d5fc6f73
洛狐 <2046665121@qq.com>
2025-05-18 12:57:41 +0800

    内容：公告：更新

--- js/index.js
+++ js/index.js
@@ -48,7 +48,16 @@ function openNotice() {
     
     title: '公告',
     
-    content: '<p class="typo">求求某些缺德的人补药再滥用我滴流量了（悲</p><p class="typo"><img src="/file/picture/公告.webp"></p>',
+    content: `
+<div class="mdui-typo">
+  <p>PojavLauncher的用户请注意！PojavLauncher开发团队于2025年5月17日宣布项目停止更新，GH代码仓库归档。这意味以后不会再有新功能开发和安全更新。大家可转向FCL启动器、ZL启动器作为替代方案，或密切关注Pojav团队全新成立的AngelAuraMC组织推出的Amethyst启动器项目。</p>
+  <hr>
+  <p>求求某些缺德的人补药再滥用我滴流量了（悲</p>
+  <p>
+    <img src="/file/picture/公告.webp">
+  </p>
+</div>
+`,
     
     buttons: [
     {
@@ -89,22 +98,22 @@ function openEgg() {
  * 主题切换
  */
 function toggleTheme() {
-    const body = document.body;
-    const isDark = body.classList.contains('mdui-theme-layout-dark');
-    
-    body.classList.toggle('mdui-theme-layout-dark', !isDark);
-    
-    localStorage.setItem('theme', isDark ? 'light' : 'dark');
+  const body = document.body;
+  const isDark = body.classList.contains('mdui-theme-layout-dark');
+  
+  body.classList.toggle('mdui-theme-layout-dark', !isDark);
+  
+  localStorage.setItem('theme', isDark ? 'light' : 'dark');
 }
 
 /**
  * 加载主题
  */
 function loadTheme() {
-    const savedTheme = localStorage.getItem('theme');
-    const body = document.body;
-    
-    if (savedTheme) {
-        body.classList.toggle('mdui-theme-layout-dark', savedTheme === 'dark');
-    }
+  const savedTheme = localStorage.getItem('theme');
+  const body = document.body;
+  
+  if (savedTheme) {
+    body.classList.toggle('mdui-theme-layout-dark', savedTheme === 'dark');
+  }
 }
