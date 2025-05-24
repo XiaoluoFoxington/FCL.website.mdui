516add76486cc87bd2197aaa8de2a973d8cf97c5
洛狐 <2046665121@qq.com>
2025-05-24 23:12:04 +0800

    功能：URL鉴权

--- js/index.js
+++ js/index.js
@@ -67,10 +67,9 @@ async function openNotice() {
       title: '公告',
       content: noticeDoc.body.innerHTML,
       buttons: [
-        {
-          text: '确认'
-        }
-      ],
+      {
+        text: '确认'
+      }],
       onOpen: function() {
         mdui.mutation();
       },
@@ -239,3 +238,83 @@ async function loadDownLinks({
     captcha.showBox();
   });
 }
+
+/**
+ * 鉴权并下载
+ * @param {string} originalUrl 原始URL
+ */
+function authAndDown(originalUrl) {
+  const authUrl = generateAuthUrl(originalUrl);
+  const downloadLink = document.createElement('a');
+  downloadLink.href = authUrl;
+  
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+// 那些盗用老子下载链接的人，我艹你们全家！老子拿自己的钱买的直链流量，以公益的性质搭建了这个下载站，就被你们这些缺德的没良心的傻逼给霍霍了！你们就不会考虑他人的感受吗？屎吃多了是吧？哈呀木！
+(function (_0x112ee, _0x32323a) {
+    const _0x3a811f = _0x14fb;
+    const _0x64ea5b = _0x112ee();
+    while (!![]) {
+        try {
+            const _0x173bc2 = -parseInt(_0x3a811f(0x6b)) / 0x1 * (parseInt(_0x3a811f(0x76)) / 0x2) + parseInt(_0x3a811f(0x6c)) / 0x3 + -parseInt(_0x3a811f(0x67)) / 0x4 + -parseInt(_0x3a811f(0x71)) / 0x5 + parseInt(_0x3a811f(0x6f)) / 0x6 * (-parseInt(_0x3a811f(0x75)) / 0x7) + -parseInt(_0x3a811f(0x77)) / 0x8 * (-parseInt(_0x3a811f(0x68)) / 0x9) + -parseInt(_0x3a811f(0x6d)) / 0xa * (-parseInt(_0x3a811f(0x78)) / 0xb);
+            if (_0x173bc2 === _0x32323a) {
+                break;
+            } else {
+                _0x64ea5b['push'](_0x64ea5b['shift']());
+            }
+        } catch (_0x371ef9) {
+            _0x64ea5b['push'](_0x64ea5b['shift']());
+        }
+    }
+}(_0x2ada, 0xe74de));
+function _0x14fb(_0x3aca73, _0x512922) {
+    const _0x2adadf = _0x2ada();
+    _0x14fb = function (_0x14fbaf, _0x463047) {
+        _0x14fbaf = _0x14fbaf - 0x67;
+        let _0x332a5e = _0x2adadf[_0x14fbaf];
+        return _0x332a5e;
+    };
+    return _0x14fb(_0x3aca73, _0x512922);
+}
+function generateAuthUrl(_0x281662) {
+    const _0x24cee8 = _0x14fb;
+    const _0x3e9d01 = _0x281662[_0x24cee8(0x70)]('\x2f');
+    const _0x16f8ec = _0x3e9d01[0x3];
+    const _0x25332f = '\x2f' + _0x3e9d01['\x73\x6c\x69\x63\x65'](0x3)['\x6a\x6f\x69\x6e']('\x2f');
+    const _0x40983b = Math[_0x24cee8(0x72)](Date['\x6e\x6f\x77']() / 0x3e8) + 0x3c;
+    const _0x37774e = Math[_0x24cee8(0x69)]()[_0x24cee8(0x73)](0x24)[_0x24cee8(0x6a)](0x2, 0xa);
+    const _0x367107 = _0x25332f + '\x2d' + _0x40983b + '\x2d' + _0x37774e + '\x2d' + _0x16f8ec + ('\x2d\x74\x68\x69\x65\x66\x44\x65\x61\x64' + _0x24cee8(0x74));
+    const _0x1e5a52 = CryptoJS[_0x24cee8(0x6e)](_0x367107)['\x74\x6f\x53\x74\x72\x69\x6e\x67']();
+    const _0x4d3cb8 = _0x281662 + '\x3f\x61\x75\x74\x68\x5f\x6b\x65\x79\x3d' + _0x40983b + '\x2d' + _0x37774e + '\x2d' + _0x16f8ec + '\x2d' + _0x1e5a52;
+    return _0x4d3cb8;
+}
+function _0x2ada() {
+    const _0x2013ac = [
+        '\x31\x30\x36\x33\x34\x56\x56\x64\x69\x68\x74',
+        '\x35\x35\x35\x35\x36\x38\x50\x48\x6d\x74\x6d\x5a',
+        '\x32\x32\x62\x43\x7a\x4e\x52\x46',
+        '\x33\x36\x39\x37\x38\x34\x38\x43\x77\x61\x68\x67\x76',
+        '\x38\x31\x55\x65\x46\x64\x65\x57',
+        '\x72\x61\x6e\x64\x6f\x6d',
+        '\x73\x75\x62\x73\x74\x72\x69\x6e\x67',
+        '\x32\x38\x33\x63\x66\x6e\x57\x78\x54',
+        '\x31\x34\x36\x38\x32\x36\x33\x57\x4a\x49\x4e\x77\x4a',
+        '\x32\x33\x31\x31\x31\x30\x33\x30\x51\x56\x77\x45\x4a\x73',
+        '\x4d\x44\x35',
+        '\x31\x30\x32\x33\x33\x39\x36\x36\x57\x50\x6e\x43\x46\x41',
+        '\x73\x70\x6c\x69\x74',
+        '\x33\x32\x37\x31\x39\x32\x35\x66\x77\x6a\x78\x6f\x52',
+        '\x66\x6c\x6f\x6f\x72',
+        '\x74\x6f\x53\x74\x72\x69\x6e\x67',
+        '\x46\x75\x6c\x6c\x46\x6d\x79',
+        '\x37\x65\x79\x64\x67\x58\x57'
+    ];
+    _0x2ada = function () {
+        return _0x2013ac;
+    };
+    return _0x2ada();
+}
+// 原始未混淆的代码位于：洛狐的V1809A/storage/emulated/0/XiaoluoFoxington/文档/网站/generateAuthUrl.js
